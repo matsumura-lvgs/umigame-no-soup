dedda9f832fa9ba1654d383bc6b6890159f7142e
matsumura-kazuyuki <minnahero1209@gmail.com>
2023-09-05 00:26:36 +0900

    [add] 型の変更

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,5 +1,5 @@
-import { Question } from "@/app/type.ts/Question";
-import { NextResponse } from "next/server";
+import { Question } from "@/app/type/Question"
+import { NextResponse } from "next/server"
 
 export async function GET() {
   const response: { result: Question[] } = {
@@ -7,23 +7,26 @@ export async function GET() {
       {
         id: 1,
         title: "title1",
+        overview: "overciew1",
         sentence: "sentence1",
         answer: "answer1",
       },
       {
         id: 2,
         title: "title2",
+        overview: "overciew2",
         sentence: "sentence2",
         answer: "answer2",
       },
       {
         id: 3,
         title: "title3",
+        overview: "overview3",
         sentence: "sentence3",
         answer: "answer3",
       },
     ],
-  };
+  }
 
-  return NextResponse.json(response);
+  return NextResponse.json(response)
 }
