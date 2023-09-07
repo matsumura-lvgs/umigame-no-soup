f1538f93a94b437ee90e1c43048fe4b42d09ffe2
matsumura-lvgs <kazuyuki.matsumura@leverages.jp>
2023-09-07 10:30:18 +0900

    [mod] 画像の大きさ変更

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -1,25 +1,26 @@
-import QuestionCard from "@/components/organisms/QuestionCard"
-import { Question, QuestionApiResponse } from "../type/Question"
+import QuestionCard from "@/components/organisms/QuestionCard";
+import { Question, QuestionApiResponse } from "../type/Question";
 
 const Top = async () => {
-  const response = await fetch("http://localhost:3000/api/questions")
+  const response = await fetch("http://localhost:3000/api/questions");
   if (!response.ok) {
-    console.error(response.status)
-    throw new Error("Network response was not ok")
+    console.error(response.status);
+    throw new Error("Network response was not ok");
   }
-  const data: QuestionApiResponse = await response.json()
-  const questions = data.result
+  const data: QuestionApiResponse = await response.json();
+  const questions = data.result;
 
   return (
-    <div>
+    <div className="flex">
       {/* 二列ずつ、レスポンシブデザインに */}
+      {/* この辺templatesに移動 */}
       {questions.map((question: Question) => (
-        <div key={question.id}>
+        <div className="flex-auto" key={question.id}>
           <QuestionCard question={question} />
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Top
+export default Top;
--- src/components/organisms/QuestionCard.tsx
+++ src/components/organisms/QuestionCard.tsx
@@ -1,29 +1,29 @@
-import { Question } from "@/app/type/Question"
-import Image from "next/image"
-import Link from "next/link"
+import { Question } from "@/app/type/Question";
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
-  question: Question
-}
+  question: Question;
+};
 const QuestionCard = ({ question }: Props) => {
   return (
-    <div className="bg-green-200 w-80 h-80 rounded-t-lg mt-4 ml-4 shadow">
-      <Link href={`/questions/${question.id}`}>
-        <Image
-          // いずれS3からの取得に変更
-          src={`/images/questionCard/test${question.id}.png`}
-          alt=""
-          width={100}
-          height={100}
-        />
-      </Link>
-      <div>
+    <Link href={`/questions/${question.id}`}>
+      <div className="bg-green-200 w-auto rounded-t-lg object-none mt-4 ml-4 shadow">
+        <div className="relative aspect-square">
+          <Image
+            // いずれS3からの取得に変更
+            src={`/images/questionCard/test${question.id}.png`}
+            alt=""
+            fill={true}
+            // className="object-cover"
+          />
+        </div>
         <h2>{question.title}</h2>
         {/* 概要はなしでおすすめ度とかで良いかなぁ */}
         <h2>{question.overview}</h2>
       </div>
-    </div>
-  )
-}
+    </Link>
+  );
+};
 
-export default QuestionCard
+export default QuestionCard;
